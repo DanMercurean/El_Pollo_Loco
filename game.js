883d6d10a350fd7eb9c3480a940458295d0dbbe4
Dan Mercurean <danymercurean@yahoo.com>
2021-01-28 13:55:47 +0100

    added addBackGroundObject function

--- game.js
+++ game.js
@@ -85,8 +85,7 @@ function drawBackground() {
 }
 
 function drawGround() {
-    ctx.fillStyle = "#FFE699";
-    ctx.fillRect(0, 375, canvas.width, canvas.height - 375);
+   
 
     if (isMovingRight) {
         bg_elements = bg_elements - 2;
@@ -96,17 +95,26 @@ function drawGround() {
         bg_elements = bg_elements + 2;
     }
 
-    let base_image = new Image();
-    base_image.src = 'grafiken/img/bg_elem_1.png';
-    if (base_image.complete) {
-        ctx.drawImage(base_image, bg_elements, 195, base_image.width * 0.6, base_image.height * 0.6);
-    };
+    addBackgroundObject('grafiken/img/bg_elem_1.png', 0, 195, 0.6, 0.4);
+    addBackgroundObject('grafiken/img/bg_elem_2.png', 450, 120, 0.6, 0.5);
+    addBackgroundObject('grafiken/img/bg_elem_1.png', 700, 255, 0.4, 0.6);
+    addBackgroundObject('grafiken/img/bg_elem_2.png', 1100, 260, 0.3, 0.2);
 
-    let base_image2 = new Image();
-    base_image2.src = 'grafiken/img/bg_elem_2.png';
-    if (base_image2.complete) {
-        ctx.drawImage(base_image2, 450 + bg_elements, 120, base_image2.width * 0.6, base_image2.height * 0.6);
+     // Draw ground
+     ctx.fillStyle = "#FFE699";
+     ctx.fillRect(0, 375, canvas.width, canvas.height - 375);
+}
+
+function addBackgroundObject(src, offsetX, offsetY, scale, opacity) {
+    if (opacity != undefined) {
+        ctx.globalAlpha = opacity;
+    }
+    let base_image = new Image();
+    base_image.src = src;
+    if (base_image.complete) {
+        ctx.drawImage(base_image, offsetX + bg_elements, offsetY, base_image.width * scale, base_image.height * scale);
     };
+    ctx.globalAlpha = 1;
 }
 
 /**
